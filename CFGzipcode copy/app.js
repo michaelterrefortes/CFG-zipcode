18d1ffa2875191a4caf48b50f385b3c95b6d9d00
Michael Terrefortes <60881801+michaelterrefortes@users.noreply.github.com>
2021-05-03 17:13:40 -0400

    Update app.js

--- CFGzipcode copy/app.js
+++ CFGzipcode copy/app.js
@@ -1,12 +1,6 @@
 var rules = {
     "start": ["#NP# #VP#."],
-    // "NP": ["#Det# #N#", "#Det# #N# that #VP#", "#Det# #Adj# #N#"],
-    // "VP": ["#Vtrans# #NP#", "#Vintr#"],
-    // "Det": ["The", "This", "That"],
-    // "N": ["John Keating", "Bob Harris", "Bruce Wayne", "John Constantine", "Tony Stark", "John Wick", "Sherlock Holmes", "King Leonidas"],
-    // "Adj": ["cool", "lazy", "amazed", "sweet"],
-    // "Vtrans": ["computes", "examines", "helps", "prefers", "sends", "plays with", "messes up with"],
-    // "Vintr": ["coughs", "daydreams", "whines", "slobbers", "appears", "disappears", "exists", "cries", "laughs"]
+  
 }
 let grammar = tracery.createGrammar(rules);
 let expansion = grammar.flatten('#start#');
@@ -17,10 +11,7 @@ function generate() {
     	"NP": ["#area# #N#"],
     	"area": ["000"],
     	"N": ["617","601","602","603","703", "705","610","611","612","921","917","925"],
-        // "NL": [""],
-    	// "Adj": ["dangerous", "Big", "pretty", "angry"],
-    	// "Vtrans": ["attaks", "licks", "helps", "prefers", "plays with", "messes up with"],
-    	// "Vintr": ["coughs", "Yawn", "Hunt", "farts", "appears", "disappears", "exists", "cries", "laughs"]
+      
     }
     
     let grammar = tracery.createGrammar(data);
